652f7512fe89409b01894e8e14ee6e2a91ba3c9c
MrRiaz <monsurrahmanriaz@gmail.com>
2020-12-12 23:05:17 +0600

    remove clg

--- src/components/pages/ClothesDetail/ClothesDetail.js
+++ src/components/pages/ClothesDetail/ClothesDetail.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import fakeData from '../../fakeData/clothesInfo';
-// import Clothes from '../Clothes/Clothes';
+
 
 const ClothesDetails = () => {
     const {productKey} = useParams();
     const product = fakeData.find(pd => pd.key === productKey);
     const {name, price, image} = product;
-    console.log(product);
     return (
         <div>
             {/* <Clothes product={product} ></Clothes> */}
             {/* <h1>{name}</h1> */}
-            <div className="align">
+            <div className="">
                 <img width="300px" src={image} alt=""/>
                 <h1 className="head1">{name}</h1>
                 <h3 className="head3">{price} </h3>
