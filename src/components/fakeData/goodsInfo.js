d1d75f7598152f24a1ed6fafc3a93bc6e1bd0bcf
MrRiaz <monsurrahmanriaz@gmail.com>
2020-12-15 02:46:03 +0600

    try to make add to cart functions

--- src/components/fakeData/goodsInfo.js
+++ src/components/fakeData/goodsInfo.js
@@ -23,44 +23,23 @@ var goodsInfo = [
     {
         image: "https://i.imgur.com/7jlY42t.jpg",
         key: "4",
-<<<<<<< HEAD
         name:"P&Co Key Ring", 
         price:"$20.00",
         category: "goods",
-=======
-        price:"$20.00",
-        category: "goods"
-        name:"Hometown Wool 5 Panel Cap", 
-        price:"$34.00",
->>>>>>> 49cd2d9e4cf88445b96763f61ef8d92bad1340f2
      },
      {
          image: "https://i.imgur.com/bTlCgMX.jpg",
          key: "5", 
-<<<<<<< HEAD
          name: "Think Free Motel Key Fob",
          price:"$20.00",
-         category: "goods"
-=======
-         price:"$20.00",
-         category: "goods"
-         name: "2 Pack Wild Souls Bandana",
-         price:"$34.00",
->>>>>>> 49cd2d9e4cf88445b96763f61ef8d92bad1340f2
+         category: "goods",
      },
      {
          image: "https://i.imgur.com/US1gH43.jpg",
          key: "6", 
-<<<<<<< HEAD
          name:"Good At Bad Decisions Leather Wallet",
          price:"$92.00",
          category: "goods"
-=======
-         price:"$92.00",
-         category: "goods"
-         name:"Bolt Beanie",
-         price:"$26.00",
->>>>>>> 49cd2d9e4cf88445b96763f61ef8d92bad1340f2
      },
     
 ]
